8f77f924e53f9d2e5b9e1b5b9dcb2f4b119b08e8
Valenciola <valeriez3012@gmail.com>
2025-12-11 23:47:36 -0500

    Setting the stage to switch

--- Scripts/audio.js
+++
@@ -1,4 +0,0 @@
-/* 
-Audio.js - The file that handles song transitions between the sections
-Leverages Howler.js; details about this library are available here: https://github.com/goldfire/howler.js
-*/
